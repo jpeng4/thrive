aaf3f30165ccfc993e1c8cfea45cdb2fb01c2806
Julia Peng <julia.peng10@gmail.com>
2018-01-19 14:35:19 -0800

    some url/view stuff

---
+++ buddies/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+	return HttpResponse("This is the buddies index")
+
+
+
+def home(request):
+	return HttpResponse("This is the home page")
